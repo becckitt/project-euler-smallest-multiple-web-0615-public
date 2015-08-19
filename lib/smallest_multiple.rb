d53fae773e7c79d706197a0661969ae4df206a62
Flatiron School <flatironschool@Flatirons-MacBook-Air.local>
2015-08-19 11:08:54 -0400

    progress

--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,17 @@
-# Implement your procedural solution here!
+# 2520 is the smallest number that can be divided 
+# by each of the numbers from 1 to 10 without any 
+# remainder.
+# What is the smallest positive number that is 
+# evenly divisible by all of the numbers from 1 to 20?
+
+def smallest_multiple(input)
+	range = (1..input)
+	range.each do |num|
+		running_total = num
+			if running_total % num != 0
+				running_total = num * running_total
+			else
+				running_total
+			end
+	end
+end
